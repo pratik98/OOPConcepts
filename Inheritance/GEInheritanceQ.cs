e29959c829dbfec10d7d3c1c44bba676e545e7d2
Pratik98 <pratik4fp@gmail.com>
2017-05-10 12:09:49 +0530

    added GE Inheritance exmaple

---
+++ Inheritance/GEInheritanceQ.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class GEInheritanceQ
+    {
+        class A
+        {
+            public int number;
+            public string name;
+
+            public A()
+            {
+                number = 10;
+                // name = "india"; // since name for A has not be assigned any value,
+                //Compiler will give warning about it being null always
+            }
+        }
+
+        class B : A
+        {
+            public new  int number; //compiler warning : hides field from class A,
+            public new string name; //Use new keyword to remove warning
+
+            public B()
+            {
+                number = 20;
+                name = "mumbai";
+            } 
+        }
+
+        class C : B
+        {
+            public new int number;//compiler warning : hides field from class B
+            public new string name;
+
+            public C()
+            {
+                number = 30;
+                name = "vadodara";
+            }
+
+            public void disp(A obj)
+            {
+                Console.WriteLine("Inside C's Disp Method:");
+                Console.WriteLine(obj.number);
+                Console.WriteLine(obj.name);
+            }
+        }
+       
+            public static void Main(string[] args)
+            {
+                A obj1 = new A();
+                Console.WriteLine(obj1.number + "" + obj1.name); // Prints 10
+
+                A obj2 = new B();
+                Console.WriteLine(obj2.number + " " + obj2.name); // Prints 10
+
+                A obj3 = new C();
+                Console.WriteLine(obj3.number + "" + obj3.name); // Prints 10
+
+                B obj4 = new B();
+                Console.WriteLine(obj4.number + "" + obj4.name); // Prints 20 Mumbai
+
+                C obj5 = new C();
+                Console.WriteLine(obj5.number + "" + obj5.name); // Prints 30 vadodara
+
+                // runtime using method
+                obj5.disp(new B()); // Prints 10
+
+                // runtime using method
+                obj5.disp(new A());
+                Console.ReadKey(); // Prints 10
+            }
+        
+    }
+}
--- Inheritance/YieldEx.cs
+++ Inheritance/YieldEx.cs
@@ -7,67 +7,67 @@ using System.Threading.Tasks;
 
 namespace Inheritance
 {
-    class YieldEx
-    {
+    //class YieldEx
+    //{
 
-        // YIELD
-        // Usage of the "yield" keyword indicates that the method it appears in is an Iterator
-        // (this means you can use it in a foreach loop)
-        public static IEnumerable<int> YieldCounter(int limit = 10)
-        {
-            for (var i = 0; i < limit; i++)
-                yield return i; // retains previous "i" value
-        }
+    //    // YIELD
+    //    // Usage of the "yield" keyword indicates that the method it appears in is an Iterator
+    //    // (this means you can use it in a foreach loop)
+    //    public static IEnumerable<int> YieldCounter(int limit = 10)
+    //    {
+    //        for (var i = 0; i < limit; i++)
+    //            yield return i; // retains previous "i" value
+    //    }
 
-        // which you would call like this :
-        public static void PrintYieldCounterToConsole()
-        {
-            foreach (var counter in YieldCounter())
-                Console.WriteLine(counter);
-        }
-        // you can use more than one "yield return" in a method
-        public static IEnumerable<int> ManyYieldCounter()
-        {
-            yield return 0;
-            yield return 1;
-            yield return 2;
-            yield return 3;
-        }
+    //    // which you would call like this :
+    //    public static void PrintYieldCounterToConsole()
+    //    {
+    //        foreach (var counter in YieldCounter())
+    //            Console.WriteLine(counter);
+    //    }
+    //    // you can use more than one "yield return" in a method
+    //    public static IEnumerable<int> ManyYieldCounter()
+    //    {
+    //        yield return 0;
+    //        yield return 1;
+    //        yield return 2;
+    //        yield return 3;
+    //    }
  
 
 
-        public static void Main(String[] args)
-        {
-            PrintYieldCounterToConsole();
+    //    public static void Main(String[] args)
+    //    {
+    //        PrintYieldCounterToConsole();
 
-              // NULLABLE TYPES - great for database interaction / return values
-            // any value type (i.e. not a class) can be made nullable by suffixing a ?
-            // <type>? <var name> = <value>
-            int? nullable = null; // short hand for Nullable<int>
-            Console.WriteLine("Nullable variable: " + nullable);
-            bool hasValue = nullable.HasValue; // true if not null
+    //          // NULLABLE TYPES - great for database interaction / return values
+    //        // any value type (i.e. not a class) can be made nullable by suffixing a ?
+    //        // <type>? <var name> = <value>
+    //        int? nullable = null; // short hand for Nullable<int>
+    //        Console.WriteLine("Nullable variable: " + nullable);
+    //        bool hasValue = nullable.HasValue; // true if not null
 
-            // ?? is syntactic sugar for specifying default value (coalesce)
-            // in case variable is null
-            int notNullable = nullable ?? 0; // 0
+    //        // ?? is syntactic sugar for specifying default value (coalesce)
+    //        // in case variable is null
+    //        int notNullable = nullable ?? 0; // 0
 
-            // ?. is an operator for null-propagation - a shorthand way of checking for null
-            //nullable?.Print(); // Use the Print() extension method if nullable isn't null
+    //        // ?. is an operator for null-propagation - a shorthand way of checking for null
+    //        //nullable?.Print(); // Use the Print() extension method if nullable isn't null
 
             
-            // DISPOSABLE RESOURCES MANAGEMENT - let you handle unmanaged resources easily.
-            // Most of objects that access unmanaged resources (file handle, device contexts, etc.)
-            // implement the IDisposable interface. The using statement takes care of
-            // cleaning those IDisposable objects for you.
-            using (StreamWriter writer = new StreamWriter("log.txt"))
-            {
-                writer.WriteLine("Nothing suspicious here");
-                // At the end of scope, resources will be released.
-                // Even if an exception is thrown.
-            }
+    //        // DISPOSABLE RESOURCES MANAGEMENT - let you handle unmanaged resources easily.
+    //        // Most of objects that access unmanaged resources (file handle, device contexts, etc.)
+    //        // implement the IDisposable interface. The using statement takes care of
+    //        // cleaning those IDisposable objects for you.
+    //        using (StreamWriter writer = new StreamWriter("log.txt"))
+    //        {
+    //            writer.WriteLine("Nothing suspicious here");
+    //            // At the end of scope, resources will be released.
+    //            // Even if an exception is thrown.
+    //        }
 
 
-            Console.ReadLine();
-        }
-    }
+    //        Console.ReadLine();
+    //    }
+    //}
 }
